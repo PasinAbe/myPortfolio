0157a3467f86824ca419413fe684d51c0a302722
PasinAbe <pasinwrites@gmail.com>
2025-12-29 21:56:37 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,10 @@
+window.addEventListener("scroll", function () {
+  let header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 100);
+});
+
+
+window.addEventListener("scroll", function () {
+  let scrool = document.querySelector("#scrool-up");
+  scrool.classList.toggle("srcl", window.scrollY > 600);
+});
